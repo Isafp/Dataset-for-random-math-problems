7bc7de6a9215f1d38ff556d4ac3af3712ec89ec1
Isafp <105232054+Isafp@users.noreply.github.com>
2022-07-23 00:17:31 -0300

    Create get_base64.py

---
+++ get_base64.py
@@ -0,0 +1,7 @@
+import base64
+@st.cache(allow_output_mutation=True)
+
+def get_base64(bin_file):
+    with open(bin_file, 'rb') as f:
+        data = f.read()
+    return base64.b64encode(data).decode()
